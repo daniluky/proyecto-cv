5754b635c832d8455fdb54f2240511ebc4f7d573
I02eccad <i02eccad@uco.es>
2025-06-25 02:21:30 +0200

    Primera version del generador de CV

---
+++ routes/cv.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const cvController = require('../controllers/cv.controller');
+
+router.post('/', cvController.crearCV);
+router.get('/', cvController.obtenerCVs);
+
+module.exports = router;
